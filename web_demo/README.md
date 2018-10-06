e9206315d1d2938e24582ea67affdd129dcca42b
Joshua Morse <joshuamorse1993@gmail.com>
2018-10-06 14:21:32 -0400

    "Hello World"-esque beginnings of web demo. Used create-react-app and added eslint

---
+++ web_demo/README.md
@@ -0,0 +1,9 @@
+To run
+
+1) install node (v8+)
+2) install npm
+3) install yarn
+4) run "yarn install"
+5) run "yarn start"
+
+enjoy!
---
+++ web_demo/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='metl-demo'>
+                <div className='title'>
+                    Demo for the mETL library
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
